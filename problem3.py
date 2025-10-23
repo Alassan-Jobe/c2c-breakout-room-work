2f991b73a63a7d45a48e60d633eece4b4e27f349
birbbled <114835834+birbbled@users.noreply.github.com>
2025-10-23 18:39:32 -0400

    finished problem3.py

--- problem3.py
+++ problem3.py
@@ -1,1 +1,15 @@
+#Identify if an inputted number is even or odd. Return is boolean. If even, return true. Otherwise, return false. No edge cases
+#if num == even:
+    #return true
+#else:
+    #return false
 
+
+def is_even(num):
+    return num % 2 == 0
+
+#testing the solution
+print(is_even(5)) #returns false
+print(is_even(8)) #returns true
+print(is_even(0)) #returns true
+print(is_even(2)) #returns true
